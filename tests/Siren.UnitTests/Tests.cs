be821400896e1194e3277197ff240abd4ff2ab71
Denis Zhukovski <46710314+DenisZhukovski@users.noreply.github.com>
2024-02-23 23:53:43 +0300

    Add Unit Tests project (#1)
    * Add Unit Tests project
    
    * Add permission to deploy test results
    
    * Add test coverage report

---
+++ tests/Siren.UnitTests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Siren.UnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
