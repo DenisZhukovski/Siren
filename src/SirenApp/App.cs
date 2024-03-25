2bd7a44f7e1afef2f56c140a42533ae7e17473a6
Denis Zhukovski <46710314+DenisZhukovski@users.noreply.github.com>
2024-03-25 10:19:08 +0300

    Update project diagrams according to latest info (#44)

--- src/SirenApp/App.cs
+++ src/SirenApp/App.cs
@@ -1,54 +1,52 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Dispatching;
+using Siren.Extensions;
+
 namespace Siren;
 
 public class App : Application
 {
     protected Window? MainWindow { get; private set; }
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    protected IHost? Host { get; private set; }
+    
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
     {
-#if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
-        MainWindow = new Window();
-#else
-        MainWindow = Microsoft.UI.Xaml.Window.Current;
+#if __IOS__ || __ANDROID__
+        FeatureConfiguration.Style.ConfigureNativeFrameNavigation();
 #endif
-
+        var builder = this.CreateBuilder(args)
+            .Configure(host => host
+#if DEBUG
+                .UseEnvironment(Environments.Development)
+#endif
+                .ConfigureServices(RegisterServices)
+                .UseNavigation(
+                    RegisterRoutes,
+                    configure: cfg => cfg with { AddressBarUpdateEnabled = true }
+                )
+            ).UseToolkitNavigation();
+        MainWindow = builder.Window;
 #if DEBUG
         MainWindow.EnableHotReload();
 #endif
-
-
-        // Do not repeat app initialization when the Window already has content,
-        // just ensure that the window is active
-        if (MainWindow.Content is not Frame rootFrame)
-        {
-            // Create a Frame to act as the navigation context and navigate to the first page
-            rootFrame = new Frame();
-
-            // Place the frame in the current Window
-            MainWindow.Content = rootFrame;
-
-            rootFrame.NavigationFailed += OnNavigationFailed;
-        }
-
-        if (rootFrame.Content == null)
-        {
-            // When the navigation stack isn't restored navigate to the first page,
-            // configuring the new page by passing required information as a navigation
-            // parameter
-            rootFrame.Navigate(typeof(MainPage), args.Arguments);
-        }
-
-        // Ensure the current window is active
-        MainWindow.Activate();
+        Host = await MainWindow.InitializeNavigationAsync(
+            () => Task.FromResult(builder.Build()),
+            initialNavigate: (sp, nav) => DispatcherQueue
+                .GetForCurrentThread()
+                .OnUIAsync(() => _ = nav.NavigateViewModelAsync<MainViewModel>(this))
+        );
     }
 
-    /// <summary>
-    /// Invoked when Navigation to a certain page fails
-    /// </summary>
-    /// <param name="sender">The Frame which failed navigation</param>
-    /// <param name="e">Details about the navigation failure</param>
-    void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+    private static void RegisterServices(HostBuilderContext context, IServiceCollection services)
     {
-        throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
+        // Nothing
+    }
+    
+    private static void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
+    {
+        views.Register(
+            new ViewMap<MainPage, MainViewModel>()
+        );
     }
 }
